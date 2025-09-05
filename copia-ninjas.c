685b909553b7a1a2512c6aa57296c73372063f68
GiullianoMartinez <giulliano.martinez.y@gmail.com>
2025-09-04 22:07:11 -0400

    update copia-ninjas

--- copia-ninjas.c
+++ copia-ninjas.c
@@ -3,119 +3,110 @@
 #include <math.h>
 #include <stdbool.h>
 
-
-long long int factorial(long int n) {
+long long int factorial(long int n)
+{
   long long int fact = 1;
-  for (int i=2; i<n+1; i++) {
-    fact*= i;
+  for (int i = 2; i < n + 1; i++)
+  {
+    fact *= i;
   }
   return fact;
 }
 
-
-<<<<<<< HEAD
-double experiencia(int n) {
+int experiencia(int n)
+{
   double suma = 0;
-=======
-int experiencia(int n) {
-  double suma = 0.0;
->>>>>>> 8246d86cde578ad4824c7b6be8ad7d2ad6a73c54
-  for (int k=0; k<n; k++) {
-<<<<<<< HEAD
-    double base = 2.0*k;
-    double exponente = 2*k+1;
-    double numerador = pow(base,exponente);
-    double denominador = factorial(k+1);
-=======
-    double numerador = pow(2*k, 2*k+1);
-    double denominador = factorial(k+1);
->>>>>>> 8246d86cde578ad4824c7b6be8ad7d2ad6a73c54
-    suma+= numerador/denominador;
+  for (int k = 0; k < n; k++)
+  {
+    double numerador = pow(2 * k, 2 * k + 1);
+    double denominador = factorial(k + 1);
+    suma += numerador / denominador;
   }
   return (int)sqrt(suma);
 }
 
-
-int espiritu(int n) {
+int espiritu(int n)
+{
   int sumaDiv = 0;
-  for (int i = 1; i < n+1; i++) {
-    if (n % i == 0 && i % 2 == 1) {
+  for (int i = 1; i < n + 1; i++)
+  {
+    if (n % i == 0 && i % 2 == 1)
+    {
       sumaDiv += i;
     }
   }
   return sumaDiv;
 }
 
-
-int chakra(int m, int n) {
+int chakra(int m, int n)
+{
   int numero = m * n;
   int sumaDigitosPares = 0;
-  while (numero > 0) {
+  while (numero > 0)
+  {
     int ultDigito = numero % 10;
-    if (ultDigito % 2 == 0) {
+    if (ultDigito % 2 == 0)
+    {
       sumaDigitosPares += ultDigito;
     }
-<<<<<<< HEAD
     numero /= 10;
-=======
-    numero/= 10;
->>>>>>> 8246d86cde578ad4824c7b6be8ad7d2ad6a73c54
   }
   return sumaDigitosPares;
 }
 
-
-bool contieneAl2(int n) {
-  while (n > 0) {
+bool contieneAl2(int n)
+{
+  while (n > 0)
+  {
     int ultDigito = n % 10;
-    if (ultDigito == 2) {
+    if (ultDigito == 2)
+    {
       return true;
     }
-<<<<<<< HEAD
     n /= 10;
-=======
-    n/= 10;
->>>>>>> 8246d86cde578ad4824c7b6be8ad7d2ad6a73c54
   }
   return false;
 }
 
-
-float calcularPorcentaje(int parcial, int total) {
-  if (total == 0) {
+float calcularPorcentaje(int parcial, int total)
+{
+  if (total == 0)
+  {
     return 0.0;
-  } else {
-    return ((float)parcial/total) * 100;
+  }
+  else
+  {
+    return ((float)parcial / total) * 100;
   }
 }
 
-
-
-int main() {
+int main()
+{
   int totalNinjas = 0, contadorPromovidos = 0, sumaIvs = 0;
 
-  while (1) {
+  while (1)
+  {
     char nombre[50];
     printf("Ingrese ninja : ");
     scanf("%49s", nombre);
 
-    if (strcmp(nombre,"FIN") == 0) {
+    if (strcmp(nombre, "FIN") == 0)
+    {
       break;
     }
 
     int mce, vsm, aef, nci;
-    scanf("%d", &mce);
-    scanf("%d", &vsm);
-    scanf("%d", &aef);
-    scanf("%d", &nci);
-    
-    int ivs = (experiencia(mce) + espiritu(vsm)) * chakra(aef,nci);
+    printf("Ingrese mce, vsm, aef, nci separados por espacio: ");
+    scanf("%d %d %d %d", &mce, &vsm, &aef, &nci);
+
+    int ivs = (experiencia(mce) + espiritu(vsm)) * chakra(aef, nci);
     sumaIvs += ivs;
 
     printf("Ninja %s \n", nombre);
     printf("Indice de voluntad shinobi = %d\n", ivs);
 
-    if (ivs > 5000 && contieneAl2(ivs)) {
+    if (ivs > 5000 && contieneAl2(ivs))
+    {
       printf("*** PROMOVIDO A JERARQUIA CHUNNIN ***\n\n");
       contadorPromovidos++;
     }
@@ -123,12 +114,12 @@ int main() {
   }
   printf("REPORTE FINAL\n============\nTotal de ninjas procesados = %d\n", totalNinjas);
 
-  if (totalNinjas > 0) {
-    int promedio = (int)sumaIvs/totalNinjas;
+  if (totalNinjas > 0)
+  {
+    int promedio = (int)sumaIvs / totalNinjas;
     float porcentaje = round(calcularPorcentaje(contadorPromovidos, totalNinjas));
     printf("\nPromedio IVS ninjas procesados = %d\n", promedio);
     printf("%.1f %% de los ninjas fueron promovidos a la jerarquía chünin\n", porcentaje);
-    
   }
   return 0;
 }
