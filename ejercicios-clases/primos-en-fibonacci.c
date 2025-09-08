fa9c98895d4bb647edcddb221e90b697d9c71d7f
Giulliano Martínez <giulliano.martinez.y@gmail.com>
2025-09-08 12:41:49 -0300

    Update primos-en-fibonacci.c

--- ejercicios-clases/primos-en-fibonacci.c
+++ ejercicios-clases/primos-en-fibonacci.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
-#include <math.h>
+
+int esPrimo(long long int n) {
+    if (n <= 1) return 0;
+    if (n == 2) return 1;
+    if (n % 2 == 0) return 0;
+
+    for (long long int i=3; i*i <=n; i+=2) {
+        if (n % i == 0) {
+            return 0;
+        } 
+    }
+    return 1;
+}
+
 
 int main() {
-    int num1 = 0, num2 = 1, resultado;
+    long long int num1 = 0, num2 = 1, resultado;
     int n, encontrados = 0;
 
-    printf("Ingrese la cantidad de números primos en la sucesión de Fibonacci: ");
+    printf("Ingrese la cantidad de números primos de Fibonacci: ");
     scanf("%d", &n);
 
     while (encontrados < n) {
@@ -13,26 +26,12 @@ int main() {
         num1 = num2;
         num2 = resultado;
 
-        // Verificar si el número es primo
-        if (resultado == 2) {
-            printf("%d\n", resultado);
+        if (esPrimo(resultado)) {
+            printf("%lld ", resultado);
             encontrados++;
-        } else if (resultado > 2) {
-            int primo = 1; // asumimos que es primo
-
-            int tope = sqrt(resultado);
-            for (int i = 2; i <= tope; i++) {
-                if (resultado % i == 0) {
-                    primo = 0; // no es primo
-                    break;
-                }
-            }
-
-            if (primo) {
-                printf("%d\n", resultado);
-                encontrados++;
-            }
         }
+    
+        
     }
 
     return 0;
