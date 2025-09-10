a412382541de562a51364736fdf7dcfd4bfa8ce2
Giulliano Martínez <giulliano.martinez.y@gmail.com>
2025-09-10 15:22:29 -0300

    Update triangulo-pascal.c

--- ejercicios-clases/triangulo-pascal.c
+++ ejercicios-clases/triangulo-pascal.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+
+int factorial(int n);
+
+int main() {
+    int n;
+    printf("Ingrese cant. de filas: ");
+    scanf("%d",&n);
+    
+    for (int filas=0; filas<=n; filas++) {
+        for (int esp=1; esp<=n-filas; esp++) {
+            printf(" ");
+        }
+        
+        for (int rep=0; rep<=filas; rep++) {
+            int num= factorial(filas) / (factorial(filas-rep) * factorial(rep));
+            printf("%d ",num);
+            
+        }
+        printf("\n");
+    }
+    
+    
+    return 0;
+}
+
+int factorial(int n) {
+    int fact = 1;
+    
+    for (int i=2; i<=n; i++) {
+        fact*= i;
+    }
+    return fact;
+}
 
